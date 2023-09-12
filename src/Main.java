f45aa6bcb07719e22ce85db5a6ffa7cfa2208a24
marksman.max <marksman.max@yandex.ru>
2023-09-12 15:23:09 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println("Вывод значений:");
         System.out.println("Dog = " + dog);
         System.out.println("Cat = " + cat);
         System.out.println("Paper = " + paper);

b6ab0d5bad89f5d1517b2db2ecc2f2a77e96a437
marksman.max <marksman.max@yandex.ru>
2023-09-12 15:19:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Dog = " + dog);
+        System.out.println("Cat = " + cat);
+        System.out.println("Paper = " + paper);
+    }
+}

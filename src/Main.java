8af458957e2103cefcf9e347daa7c860770cb906
marksman.max <marksman.max@yandex.ru>
2023-09-12 15:34:00 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
+        // 1 задание
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
 
+        // 2 задание
         dog += 4;
         cat += 4;
         paper += 4;
+
+        // 3 задание
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+
+        // 4 задание
+
+
+
         System.out.println("Вывод значений:");
         System.out.println("Dog = " + dog);
         System.out.println("Cat = " + cat);
